a22651e8c7246d10d2834fb11ee658264fd10a1d
Fanny Lau <fannylauchen7@gmail.com>
2026-03-01 17:24:32 +0100

    RouterView desarrollado

---
+++ src/pages/portfolios/Animation.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+    <h1>Animation</h1>
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,11 @@
+import Personalbio from "@/pages/bio/PersonalBio.vue";
+import ProfessionalBio from "@/pages/bio/ProfessionalBio.vue";
+import Contact from "@/pages/contact/Contact.vue";
 import Home from "@/pages/home/Home.vue";
+import Animation from "@/pages/portfolios/Animation.vue";
+import Dibujos from "@/pages/portfolios/Dibujos.vue";
+import Editorial from "@/pages/portfolios/Editorial.vue";
+import Illustration from "@/pages/portfolios/Illustration.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 
@@ -13,8 +20,44 @@ export const router = createRouter({
       component: Home
     },
     {
-      path: '/:patchMatch(.*)',
+      path: '/animation',
+      name: 'animation',
+      component: Animation
+    },
+    {
+      path: '/illustration',
+      name: 'illustration',
+      component: Illustration
+    },
+    {
+      path: '/editorial',
+      name: 'editorial',
+      component: Editorial
+    },
+    {
+      path: '/dibujos',
+      name: 'dibujos',
+      component: Dibujos
+    },
+    {
+      path: '/personalbio',
+      name: 'personalbio',
+      component: Personalbio
+    },
+    {
+      path: '/professionalbio',
+      name: 'professionalbio',
+      component: ProfessionalBio
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
+    {
+      path: '/:pathMatch(.*)*',
       redirect: '/'
     }
+    
   ]
 })
